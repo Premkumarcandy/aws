c1e91b72576d4c7496dad7f42879662605c0993a
Anton Raizvikh <raizvih@gmail.com>
2016-08-26 11:54:10 -0400

    add web playbook

--- up.sh
+++ up.sh
@@ -4,8 +4,9 @@
 ansible-playbook vpc.yml -e '@vars/devbrd.yml'
 ansible-playbook redis.yml -e '@vars/devbrd.yml'
 ansible-playbook rds.yml -e '@vars/devbrd.yml'
-
-# ansible-playbook ec2_keys.yml -e '@vars/devbrd.yml'
-#    - elb
+#
+ansible-playbook elb.yml -e '@vars/devbrd.yml'
+ansible-playbook ec2_keys.yml -e '@vars/devbrd.yml'
+#
 #    - web
 #    - create_db
