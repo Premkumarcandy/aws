4d8c8e5aafee746d706f5744b6575f24988fbc4c
Anton Raizvikh <raizvih@gmail.com>
2016-08-30 10:26:44 -0400

    update deploy playbook

--- up.sh
+++ up.sh
@@ -7,7 +7,13 @@ ansible-playbook rds.yml -e '@vars/devbrd.yml'
 #
 ansible-playbook elb.yml -e '@vars/devbrd.yml'
 ansible-playbook ec2_keys.yml -e '@vars/devbrd.yml'
+#
+#ansible-playbook -v iam.yml -e '@vars/devbrd.yml' -e '@vars/cityline.yml'
+# rename to provision ec2 role
 ansible-playbook web.yml -e '@vars/devbrd.yml'
-# 
+#
 ansible-playbook -v db.yml -e '@vars/devbrd.yml' -e '@vars/cityline.yml'
-
+ansible-playbook -v s3.yml -e '@vars/devbrd.yml' -e '@vars/cityline.yml'
+#
+# rename to web role
+ansible-playbook -v deploy.yml -e '@vars/devbrd.yml' -e '@vars/cityline.yml'
